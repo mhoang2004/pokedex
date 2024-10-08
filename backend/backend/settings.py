44e97a90ed8a65858aa0ff435dcad3f4f55c18e2
Hoang Le <you@example.com>
2024-10-08 23:07:51 +0700

    requirements.txt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b#mp#^32qwr!6z+!+1l&c37gmuf)dwvc7hk3@hla7gt=g2#3$v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
